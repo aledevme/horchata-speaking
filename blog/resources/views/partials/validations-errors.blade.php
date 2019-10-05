7ca0a4e6dd788bd3072060abcec56f3e6b36f771
Alejandro <alexgve7@gmai.com>
2019-10-04 21:38:00 -0600

    alert messages

--- blog/resources/views/partials/validations-errors.blade.php
+++ blog/resources/views/partials/validations-errors.blade.php
@@ -1,7 +1,10 @@
 @if($errors->any())
-        <ul>
-            @foreach ($errors->all() as $itemError)
-                <li> {{$itemError}} </li>
-            @endforeach
-        </ul>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <ul class="m-0">
+        @foreach ($errors->all() as $itemError)
+            <li> {{$itemError}} </li>
+        @endforeach
+    </ul>
+</div>
+      
 @endif
--- blog/resources/views/projects/edit.blade.php
+++ blog/resources/views/projects/edit.blade.php
@@ -4,9 +4,11 @@
     <div class="container">
         <h1>Editar proyecto</h1>
         @include('partials.validations-errors')
-        <form action="{{ route('projects.update',$project) }}" method="POST">
+        <form action="{{ route('projects.update', $project) }}" method="POST">
+            @csrf
             @method('PATCH')
             @include('projects._form',['btnText'=>'Editar'])
         </form>
+        <a href="{{ route('projects.index') }}" class="btn btn-primary">Regresar</a>
     </div>
 @endsection
